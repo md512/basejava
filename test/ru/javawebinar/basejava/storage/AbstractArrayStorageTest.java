0776330f1450bc4a88f07a64a36a87a081213925
md512 <dnm78911@gmail.com>
2023-07-31 22:37:31 +0300

    implement abstract array storage test

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -3,10 +3,10 @@ package ru.javawebinar.basejava.storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
-
-import static org.junit.Assert.*;
 
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
@@ -53,21 +53,51 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void save() throws Exception {
-
+        Resume testResume = new Resume("uuid4");
+        storage.save(testResume);
+        Assert.assertEquals(testResume, storage.get("uuid4"));
     }
 
     @Test
     public void delete() throws Exception {
-
+        storage.delete(UUID_1);
+        Assert.assertEquals(2, storage.size());
     }
 
     @Test
     public void get() throws Exception {
-
+        Assert.assertEquals(new Resume(UUID_1), storage.get(UUID_1));
     }
 
     @Test(expected = NotExistStorageException.class)
     public void getNotExist() throws Exception {
         storage.get("dummy");
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExist() throws Exception {
+        storage.update(new Resume("dummy"));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() throws Exception {
+        storage.delete("uuid4");
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExist() throws Exception {
+        storage.save(new Resume(UUID_1));
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        for (int i = 3; i < 10000; i++) {
+            try {
+                storage.save(new Resume());
+            } catch (StorageException e) {
+                Assert.fail("Storage overflow ahead of time");
+            }
+        }
+        storage.save(new Resume());
     }
 }
