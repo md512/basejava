0e9e79c7ebfde405b7333214ed2dce051d196bda
md512 <dnm78911@gmail.com>
2023-08-07 11:28:43 +0300

    add ListStorage

---
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -0,0 +1,59 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+
+    protected List<Resume> storage = new ArrayList<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        Resume[] result = new Resume[size()];
+        return storage.toArray(result);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        int index = (Integer) searchKey;
+        return index >= 0;
+    }
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return Collections.binarySearch(storage, new Resume(uuid));
+    }
+
+    @Override
+    protected Resume getElement(Object searchKey) {
+        return storage.get((Integer) searchKey);
+    }
+
+    @Override
+    protected void deleteElement(Object searchKey) {
+        storage.remove((int) searchKey);
+    }
+
+    @Override
+    protected void updateElement(Object searchKey, Resume resume) {
+        storage.set((Integer) searchKey, resume);
+    }
+
+    @Override
+    protected void saveElement(Object searchKey, Resume resume) {
+        storage.add(resume);
+    }
+}
