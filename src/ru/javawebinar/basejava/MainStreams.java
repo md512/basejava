e39c3e1f5dafec2da1ee5bc83d64ecdcf0f76a9e
md512 <dnm78911@gmail.com>
2023-11-30 15:49:02 +0300

    implement minValue

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -1,17 +1,24 @@
 package ru.javawebinar.basejava;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainStreams {
     public static void main(String[] args) {
+        int[] test = new int[]{1, 2, 3, 3, 2, 7, 7, 9};
+        System.out.println(minValue(test));
 
     }
 
     public static int minValue(int[] values) {
-        return 0;
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (x, y) -> x * 10 + y);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers){
+
         return null;
     }
 }
