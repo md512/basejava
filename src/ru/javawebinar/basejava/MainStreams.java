0e3f65645fd8dde2a003e402485150d64f98c749
md512 <dnm78911@gmail.com>
2023-12-01 08:48:24 +0300

    implement oddOrEven

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -1,13 +1,17 @@
 package ru.javawebinar.basejava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] test = new int[]{1, 2, 3, 3, 2, 7, 7, 9};
+        int[] test = new int[]{1, 2, 3, 3, 2, 3};
         System.out.println(minValue(test));
 
+        List<Integer> testList = Arrays.stream(test).boxed().collect(Collectors.toList());
+        System.out.println(oddOrEven(testList));
     }
 
     public static int minValue(int[] values) {
@@ -17,8 +21,12 @@ public class MainStreams {
                 .reduce(0, (x, y) -> x * 10 + y);
     }
 
-    public static List<Integer> oddOrEven(List<Integer> integers){
-
-        return null;
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream()
+                .reduce(Integer::sum)
+                .get();
+        return integers.stream()
+                .filter(x -> sum % 2 == 0 && x % 2 == 0 || sum % 2 != 0 && x % 2 != 0)
+                .collect(Collectors.toList());
     }
 }
